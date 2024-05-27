5712d7b4311067a79ee45b3054371ea97c9d481f
martuft1 <martinafallastaiah@gmail.com>
2024-05-27 14:07:47 -0300

    Contraseña funcional creo

--- Assets/Loginmaster.cs
+++ Assets/Loginmaster.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Loginmaster : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    //
+    public string contraseña = "hola";
+    public InputField contra;
+  public void BotonAceptar()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (contra.text == contraseña)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Acces Denied");
+        }
     }
 }
